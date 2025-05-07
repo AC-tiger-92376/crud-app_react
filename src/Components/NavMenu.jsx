a561daa30e60564542d69b54208b44efa34c9e00
AC-tiger-92376 <actiger1215@gmail.com>
2025-05-06 20:22:23 -0700

    change_NavLink

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from 'react-router-dom';
 
 const NavMenu = () => {
   return (
@@ -11,9 +11,9 @@ const NavMenu = () => {
         <Container>
           <Navbar.Brand href="#home" >Student Management System</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="resultPage">Results</Nav.Link>
-            <Nav.Link href="aboutMe">About</Nav.Link>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/resultPage">Results</Nav.Link>
+            <Nav.Link as={Link} to="/aboutMe">About</Nav.Link>
           </Nav>
         </Container>
         </Navbar>
