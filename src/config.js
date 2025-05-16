0ee7c977a4888fe3ad0a57a351bf9ec416851057
AC-tiger-92376 <actiger1215@gmail.com>
2025-05-16 05:05:05 -0700

    User_complete

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-export const API_STUDENTS_URL = `${API_BASE_URL}/Students`;
+export const API_STUDENTS_URL = `${API_BASE_URL}/Student`;
 export const API_AUTH_URL = `${API_BASE_URL}/Auth`;
